1c324f5743d9dd86f6c7e84da8f915b3ef5b7173
William Fox <wfox96@gmail.com>
2023-12-25 23:20:03 -0800

    init

---
+++ src/genius_client/client.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+import os
+import typing as t
+from urllib.parse import urljoin
+
+import requests
+
+import genius_client.models as m
+
+_SortType = t.Literal["title", "popularity"]
+_TextFormatType = t.Literal["dom", "plain", "html"]
+_PaginatedMethodType = t.Literal["artist_songs", "referents"]
+
+
+class GeniusClient(requests.Session):
+    base_url = "https://api.genius.com"
+
+    def __init__(self, access_token: str | None = None, per_page: int = 10):
+        super().__init__()
+        self.per_page = per_page
+        self.access_token = access_token or os.environ["GENIUS_ACCESS_TOKEN"]
+        self.headers.update({"Authorization": f"Bearer {self.access_token}"})
+
+    def genius_request(self, method: str, url: str, params: dict, **kwargs) -> dict:
+        response = self.request(method, urljoin(self.base_url, url), params=params, **kwargs)
+        return response.json()["response"]
+
+    def genius_get(self, url, params: dict) -> dict:
+        return self.genius_request("GET", url=url, params=params, allow_redirects=True)
+
+    def paginated_get(self, url: str, params: dict) -> dict:
+        params.setdefault("page", 1)
+        params.setdefault("text_format", "plain")
+        return self.genius_get(url, params=params)
+
+    def annotations(self, annotations_id: str | int, text_format: _TextFormatType = "plain") -> m.Annotation:
+        """https://docs.genius.com/#annotations-h2"""
+        path = f"/annotations/{annotations_id}"
+        response = self.genius_get(path, params={"text_format": text_format})
+        return m.Annotation.model_validate(response["annotation"])
+
+    def referents(self, song_id: str = "", web_page_id: str = "", **kwargs) -> tuple[str | None, list[m.Referent]]:
+        """https://docs.genius.com/#/referents-h2"""
+        if not bool(song_id) ^ bool(web_page_id):
+            msg = "You may pass only one of song_id and web_page_id, not both"
+            raise ValueError(msg)
+        if song_id:
+            kwargs["song_id"] = song_id
+        if web_page_id:
+            kwargs["web_page_id"] = web_page_id
+        response = self.paginated_get("/referents", **kwargs)
+        next_page = kwargs.get("page", 1) + 1 if response["referents"] else None
+        return next_page, [m.Referent.model_validate(r) for r in response["referents"]]
+
+    def songs(self, song_id: str, text_format: _TextFormatType = "plain") -> m.FullSong:
+        """https://docs.genius.com/#/songs-h2"""
+        path = f"/songs/{song_id}"
+        response = self.genius_get(path, params={"text_format": text_format})
+        return m.FullSong.model_validate(response["song"])
+
+    def artists(self, artist_id: str, text_format: _TextFormatType = "plain") -> m.Artist:
+        """https://docs.genius.com/#/artists-h2"""
+        path = f"/artists/{artist_id}"
+        response = self.genius_get(path, params={"text_format": text_format})
+        return m.Artist.model_validate(response["artist"])
+
+    def artists_songs(self, artist_id: str, sort: _SortType = "popularity", **kwargs) -> tuple[str | None, list[m.Song]]:
+        """https://docs.genius.com/#/artists-h2"""
+        path = f"/artists/{artist_id}/songs"
+        response = self.paginated_get(path, params={"sort": sort, **kwargs})
+        return response.get("next_page"), [m.Song.model_validate(song) for song in response["songs"]]
+
+    def web_pages(self, raw_annotatable_url: str = "", canonical_url: str = "", og_url: str = "") -> m.WebPage:
+        """https://docs.genius.com/#/web_pages-h2"""
+        params = {"raw_annotatable_url": raw_annotatable_url, "canonical_url": canonical_url, "og_url": og_url}
+        params = {k: v for k, v in params.items() if v}
+        if not params:
+            msg = "One or more urls must be provided"
+            raise ValueError(msg)
+        return m.WebPage.model_validate(self.genius_get("/web_pages/lookup", params=params)["web_page"])
+
+    def search(self, q: str) -> list[dict]:
+        """https://docs.genius.com/#/search-h2"""
+        response = self.genius_get("/search", params={"q": q})
+        return [hit["result"] for hit in response["hits"]]
+
+    def user(self, user_id: str, text_format: _TextFormatType = "plain") -> m.User:
+        path = f"/users/{user_id}"
+        return m.User.model_validate(self.genius_get(path, params={"text_format": text_format})["user"])
+
+    def account(self, text_format: _TextFormatType = "plain") -> m.Me:
+        """https://docs.genius.com/#/account-h2"""
+        return m.Me.model_validate(self.genius_get("/account", params={"text_format": text_format})["user"])
+
+    def paginator(self, method: _PaginatedMethodType, **kwargs) -> t.Iterable[dict]:
+        """A Paginator for /artists/:id/songs and /referents"""
+        next_page, results = getattr(self, method)(**kwargs)
+        yield results
+        while next_page:
+            kwargs["page"] += 1
+            next_page, results = getattr(self, method)(**kwargs)
+            yield results
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def __hash__(self):
+        return hash(self.base_url + self.access_token)
+
+
+if __name__ == "__main__":
+    client = GeniusClient()
+    res = client.account()
---
+++ src/genius_client/models.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+from enum import Enum
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class UserRoleEnum(str, Enum):
+    regulator = "regulator"
+    verified_artist = "verified_artist"
+    moderator = "moderator"
+    editor = "editor"
+    mediator = "mediator"
+    contributor = "contributor"
+    staff = "staff"
+
+
+class GeniusBaseModel(BaseModel):
+    model_config = ConfigDict(coerce_numbers_to_str=True, extra="allow")
+    genius_id: str = Field(alias="id")
+    api_path: str
+    url: str
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({{}}, genius_id='{self.genius_id}')"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash(self.__class__.__name__ + str(id))
+
+
+class Annotation(GeniusBaseModel):
+    body: dict
+    share_url: str
+    state: str
+    verified: bool
+    authors: list[Author]
+    cosigned_by: list[dict]
+    verified_by: User | None
+
+    def __repr__(self):
+        return super().__repr__().format(f"state='{self.state}'")
+
+
+class Author(BaseModel):
+    attribution: float
+    pinned_role: str | None
+    user: User
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+
+class Referent(GeniusBaseModel):
+    referent_type: str = Field(alias="_type")
+    fragment: str
+    annotator_id: str
+    annotator_login: str
+    classification: str
+    referent_range: dict = Field(alias="range")
+    song_id: str
+    verified_annotator_ids: list
+    annotatable: AnnotatableSong | WebPage
+    annotations: list[Annotation]
+
+    def __repr__(self):
+        return super().__repr__().format(f"fragment='{self.fragment}'")
+
+
+class Artist(GeniusBaseModel):
+    image_url: str
+    is_verified: bool
+    header_image_url: str
+    name: str
+
+    def __repr__(self):
+        return super().__repr__().format(f"name='{self.name}'")
+
+
+class CustomPerformance(BaseModel):
+    label: str
+    artists: list[Artist]
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+
+class Album(GeniusBaseModel):
+    full_title: str
+    cover_art_url: str
+    artist: Artist
+
+    def __repr__(self):
+        return super().__repr__().format(f"full_title='{self.full_title}'")
+
+
+class AnnotatableSong(GeniusBaseModel):
+    client_timestamps: dict
+    context: str
+    image_url: str
+    title: str
+    annotatable_type: str = Field("type")
+
+    def __repr__(self):
+        return super().__repr__().format(f"title='{self.title}'")
+
+
+class Song(GeniusBaseModel):
+    """For Songs returned by /search or /artists/:id/songs"""
+
+    title: str
+    full_title: str
+    artist_names: str
+    header_image_url: str
+    header_image_thumbnail_url: str
+    song_art_image_url: str
+    song_art_image_thumbnail_url: str
+    release_date_components: dict | None = None
+    release_date_for_display: str | None = None
+    release_date_with_abbreviated_month_for_display: str | None = None
+    stats: dict
+    featured_artists: list[Artist]
+    primary_artist: Artist
+
+    def __repr__(self):
+        return super().__repr__().format(f"full_title='{self.full_title}'")
+
+
+class FullSong(Song, GeniusBaseModel):
+    # /songs/<id> fields
+
+    album: Album
+    media: list[dict]
+    producer_artists: list[Artist]
+    writer_artists: list[Artist]
+    custom_performances: list[CustomPerformance]
+    song_relationships: list[dict]
+
+
+class WebPage(GeniusBaseModel):
+    title: str
+    domain: str
+    share_url: str
+    normalized_url: str
+    annotation_count: int
+
+    def __repr__(self):
+        return super().__repr__().format(f"title='{self.title}'")
+
+
+class User(GeniusBaseModel):
+    avatar: dict
+    photo_url: str | None = None
+    header_image_url: str
+    custom_header_image_url: str | None = None
+    iq: int | None = None
+    iq_for_display: str | None = None
+    login: str
+    name: str
+    about_me: dict | None = None
+    followers_count: int | None = None
+    followed_users_count: int | None = None
+    human_readable_role_for_display: str | None = None
+    role_for_display: UserRoleEnum | None
+    roles_for_display: list[UserRoleEnum] | None = None
+    artist: Artist | None = None
+    current_user_metadata: dict
+    stats: dict | None = None
+
+    def __repr__(self):
+        return super().__repr__().format(f"name='{self.name}', login='{self.login}'")
+
+
+class Me(User, GeniusBaseModel):
+    email: str
+    unread_messages_count: int
+    unread_groups_inbox_count: int
+    unread_main_activity_inbox_count: int
+    unread_newsfeed_inbox_count: int
